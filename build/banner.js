66260db1080a20936110df083f05e3de41be7971
braun <braun@brauns-iMac.local>
2014-05-11 19:34:03 -0400

    ripped out grunt

--- build/banner.js
+++
@@ -1,9 +0,0 @@
-module.exports = function () {
-	return '/*! ' + 
-	  '<%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-    '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-    '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-    '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-    ' Licensed MIT ' + 
-    '*/\n';
-}

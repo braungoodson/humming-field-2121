12f3155e9b1a649a748608e8edf0c18c2e91af8b
braun <braun@brauns-iMac.local>
2014-05-11 19:29:32 -0400

    added files

---
+++ build/banner.js
@@ -0,0 +1,9 @@
+module.exports = function () {
+	return '/*! ' + 
+	  '<%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+    '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+    '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+    '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+    ' Licensed MIT ' + 
+    '*/\n';
+}

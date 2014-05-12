449a62aa7fd092110c54d167a283a68145c494ce
Infinite Flux Master <braungoodson@gmail.com>
2014-05-12 18:34:53 -0400

    refactor

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express'),
+  mustacheExpress = require('mustache-express'),
+port = process.env.PORT || 30000;
   app = express(),
   assetsRoot = __dirname + '/assets',
-  mustacheExpress = require('mustache-express'),
-  fs = require('fs'),
   templatesRoot = __dirname + '/templates',
   bowerRoot = __dirname + '/bower_components';
 
@@ -33,7 +33,8 @@ app.get('/', function (q,r) {
       body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
     }]
   };
-  r.render('index.mustache',viewModel);
+  r.render('index.mustache',viewModel,{partial:});
 });
 
-app.listen(process.env.PORT||30000);
+app.listen(port);
+console.log('http://localhost:'+port);
